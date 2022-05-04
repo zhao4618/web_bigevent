00a50424ec6c376e90a63c162d2ee51d9ff017e5
zhaozhuo <2424318315@qq.com>
2022-05-04 13:31:25 +0800

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,42 @@
 $(function() {
     $('#link_reg').click(function() {
-        $('#link_reg').hide()
+        $('.login_box').hide()
         $('.reg_box').show()
     })
     $('#link_login').click(function() {
         $('.reg_box').hide()
-        $('#link_reg').show()
+        $('.login_box').show()
+    })
+    let form = layui.form
+    let layer = layui.layer
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function(value) {
+            let val = $('.reg_box [name=password]').val()
+            if (val !== value) return '两次密码不一致'
+        }
+    })
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        let data = $(this).serialize()
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://www.liulongbin.top:3007/api/reguser',
+            data: data,
+            // username: $('#form_reg [name=username]').val(),
+            // password: $('#form_reg [name=password]').val()
+
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                console.log(res);
+                layer.msg('注册成功！', { icon: 6 })
+                $('#link_login').click()
+            }
+        })
     })
 })
